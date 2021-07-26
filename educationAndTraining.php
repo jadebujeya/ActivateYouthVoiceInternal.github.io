650f6526402a398442e86576282bb21a129db078
jadebujeya <78665183+jadebujeya@users.noreply.github.com>
2021-07-26 16:30:59 +1000

    Version 1 Launch (3)

--- educationAndTraining.php
+++ educationAndTraining.php
@@ -1,10 +1,25 @@
+<?php
+	include_once 'head.php';
+?>
 
-<br>
-<footer>
-<a href = "#TOP";><p style="text-align:center; border:None; color:pink";>Back to Top</p></a>
-</footer>
 
-</body>
-</html>
+<h2>Education and Training Opertunities</h2>
+<p>
+This is intended as a centralised location to post any training or personal devellopment oppertunities anyone stubles accross and thinks are worth shring, but that we are not orgainising to do as a group. 
+</p>
 
-<!-- Air America Regular  -->
+<form action="user_written_content/education_and_training/upload.php" enctype="multipart/form-data">
+  <fieldset>
+    <legend>Personalia:</legend>
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname" value="John"><br>
+    <label for="lname">Include File:</label><br>
+    <input type="file" name="file"><br><br>
+    <button type="submit" name="submit">Upload</button>
+  </fieldset>
+</form>
+
+
+<?php 
+include_once 'foot.php';
+?>
--- foot.php
+++ foot.php
@@ -1,37 +1,10 @@
-<?php
-	session_start();
-?>
 
-<!DOCTYPE html>
-<html lang="en-AU">
-<head>
-<title>Activate Youth Voice Internal</title>
-<link rel="stylesheet" href="AYVstyle.css">
-<meta http-equiv="refresh" content="300">
-<meta name="viewport" content="user-scalable=no,width=device-width,initial-scale=1.0,maximum-scale=1.0">
-<!-- look up <base> you're going to need it, specifies base url for page-->
-</head>
-<body>
-<!--<img src="AYVLogo.jpg" alt="AYV Logo">
-<br>-->
+<br>
+<footer>
+<a href = "#TOP";><p style="text-align:center; border:None; color:pink";>Back to Top</p></a>
+</footer>
 
-<h1 id = "TOP"; style="font-size: min(9vw,60px);">Internal Information Page </h1>
+</body>
+</html>
 
-<?php
-if(!isset($_SESSION["useruid"])){
-	header("location: login_and_rego/loginForm.php?error=notloggedin"); 
-	exit();
-}
-?>
-
-<div class="menu">
-	<ul>
-	  <li><a href="index.php">Home</a></li>
-	  <li><a href="projects.php">Current Projects</a></li>
-	  <li><a href="internalWork.php">House Keeping</a></li>
-	  <li><a href="contactInfo.php">Contact Info</a></li>
-	  <li><a href="ourFuture.php">Our Future</a></li>
-	  <li><a href="educationAndTraining.php">Education and Training</a></li>
-	  <li><a href="login_and_rego/includes/logout.inc.php">Logout</a></li>
-	</ul>
-</div>
+<!-- Air America Regular  -->
