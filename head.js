29fd71c000faa831702b07e302f9f217a90b55e4
jadebujeya <78665183+jadebujeya@users.noreply.github.com>
2021-07-26 15:20:26 +1000

    Create head.js

---
+++ head.js
@@ -0,0 +1,14 @@
+document.write(`
+<h1 id = "TOP"; style="font-size: min(9vw,60px);">Internal Information Page </h1>
+
+<div class="menu">
+	<ul>
+	  <li><a href="index.html">Home</a></li>
+	  <li><a href="projects.html">Current Projects</a></li>
+	  <li><a href="internalWork.html">House Keeping</a></li>
+	  <li><a href="contactInfo.html">Contact Info</a></li>
+	  <li><a href="ourFuture.html">Our Future</a></li>
+	  <li><a href="educationAndTraining.html">Education and Training</a></li>
+	</ul>
+</div>
+`);
